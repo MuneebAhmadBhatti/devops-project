b4c6f431421f2ef6fea14af8e96cbd1e7cfe49d8
MuneebAhmadBhatti <94634630+MuneebAhmadBhatti@users.noreply.github.com>
2025-05-18 13:53:38 -0700

    Initial

---
+++ bezkoder-app/app/config/db.config.js
@@ -0,0 +1,11 @@
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+} = process.env;
+
+module.exports = {
+  url: `mongodb+srv://muneebahmadbhatti786:tGkHGTne1bwA9oMw@bhatti-devops.2fwid3v.mongodb.net/`
+};
